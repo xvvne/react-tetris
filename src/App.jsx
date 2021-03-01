da95613772dfbbffb34879bbe92af6679c947cd3
Daniel Michael <itzdanielmichael@gmail.com>
2021-02-28 22:36:39 -0500

    upd App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import * as React from "react"; // airbnb style guide is gonna hate me for this one
-// import Tetris from './components/Tetris'
+import Tetris from "./screens/Tetris";
 
-const App = () => <div className="App">{/* <Tetris> */}</div>;
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+);
 
 export default App;
